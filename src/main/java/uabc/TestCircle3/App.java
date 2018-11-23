6dc8dc7a57f90633a74ea234a10094c9a52935be
castorga <cristian.astorga@uabc.edu.mx>
2018-11-23 09:56:59 -0800

    Actualizacion

---
+++ src/main/java/uabc/TestCircle3/App.java
@@ -0,0 +1,13 @@
+package uabc.TestCircle3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
